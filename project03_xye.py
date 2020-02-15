f3344816bacf1adecff101f43d9fb4b2cb972798
jeny-xye <57339127+jeny-xye@users.noreply.github.com>
2020-02-15 16:58:03 -0500

    Update

--- project03_xye.py
+++ project03_xye.py
@@ -56,7 +56,6 @@ def parse_GEDCOM(path):
                 level = list_line[0]
 
                 if level == '0':
-
                     s_tag = list_line[len(list_line)-1]
                     """ s_tag: the last word in the line, it used to judge 'INDI' or 'FAM"""
                     del list_line[0]
@@ -155,7 +154,6 @@ def parse_GEDCOM(path):
 
                 elif item[0][0] == 'FAM':
                     n = item.pop(0)
-
                     for q in item:
                         if q[0] == 'CHIL':
                             child_s.add(q[1])
@@ -179,41 +177,33 @@ if __name__ == "__main__":
 
     path = input("please input the .ged file path:")
     indi, fam = parse_GEDCOM(path)
-
     table_indi = PrettyTable(
         ['ID', 'Name', 'Gender', 'Birthday', 'Alive', 'Death', 'Child', 'Spouse'])
     table_fam = PrettyTable(['ID', 'Married', 'Divorced', 'Husband ID',
                              'Husband Name', 'Wife ID', 'Wife Name', 'Children'])
 
     for key, value in indi.items():
-
         if value['DEAT'] == 'N/A':
             alive = 'TRUE'
         else:
             alive = 'FALSE'
-
         table_indi.add_row([key, value['NAME'], value['SEX'], value['BIRTH'],
                             alive, value['DATE'], value['FAMC'], value['FAMS']])
 
     for key, value in fam.items():
-
         id_h = value['HUSB']
-
         id_w = value['WIFE']
         """ husband id and wife id """
-        
         if id_h != 'N/A':
             name_h = indi[id_h]['NAME']
         else:
             name_h = 'N/A'
         """ get husband's name"""
-        
         if id_w != 'N/A':
             name_w = indi[id_w]['NAME']
         else:
             name_w = 'N/A'
         """ get wife's name"""
-
         table_fam.add_row([key, value['MARR'], value['DATE'],
                            id_h, name_h, id_w, name_w, value['CHIL']])
 
