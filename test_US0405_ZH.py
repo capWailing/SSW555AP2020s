d7ff41067cd7c5c6b6e84c94c8f3aa43474dbd71
zhihuang-huang <57368787+zhihuang-huang@users.noreply.github.com>
2020-03-01 13:38:05 -0500

    Add files via upload

---
+++ test_US0405_ZH.py
@@ -0,0 +1,23 @@
+import unittest
+from unittest.mock import patch
+from Project04_xye import parse_GEDCOM
+from US0405_ZH import USO4, US05
+
+
+class Test(unittest.TestCase):
+
+    def test_US04(self):
+        m, n = parse_GEDCOM('test_US0405.ged')
+        with patch('builtins.print') as mocked_print:
+            US04(n)
+            mocked_print.assert_call_with('Error: FAMILY: US04: 55: F1: Divorced 18 May 1888 before married 17 FEB 1966')
+
+    def test_US05(self):
+        m, n = parse_GEDCOM('test_US0405.ged')
+        with patch('builtins.print') as mocked_print:
+            US05(m, n)
+            mocked_print.assert_call_with("Error: FAMILY: US05: 68: F3: Married 5 JAN 1992 after husband's (I07) death on 11 DEC 1991")
+
+
+if __name__ == "__main__":
+    unittest.main(exit=False, verbosity=2)
