977c3aa67d7bf78c469e5d692370b5f0e83aee32
jeny-xye <57339127+jeny-xye@users.noreply.github.com>
2020-03-01 02:14:24 -0500

    update project04

--- Project04_xye.py
+++ Project04_xye.py
@@ -1,10 +1,12 @@
 """
-project03
-Date: 02.14.2020
+project04
+Author:Xinyi Ye, Boyu Wang
+Date: 02.27.2020
 """
 from prettytable import PrettyTable
 from collections import defaultdict
 import US0203_xye
+import US12
 
 
 def parse_GEDCOM(path):
@@ -25,9 +27,9 @@ def parse_GEDCOM(path):
             """
             dicy_indi, dict_fam dictionaries: collect the results of data in .ged file
             """
-            child_s = set()
-            famc_s = set()
-            fams_s = set()
+            child_s = []
+            famc_s = []
+            fams_s = []
             """
             child_s sets: collect children in FAM level, 
             famc_s, fams_s sets: collect children and spouses in INDI level
@@ -48,9 +50,9 @@ def parse_GEDCOM(path):
             feat_IND, feat_FAM are defaultdict, the default value is 'N/A'
             used to record characteristics of INDI and FAM excepting ID
             """
-
+            line_num = 0
             for line in fp:
-
+                line_num += 1
                 line = line.rstrip('\n').rstrip(' ')
                 list_line = line.split(' ')
                 level = list_line[0]
@@ -69,11 +71,13 @@ def parse_GEDCOM(path):
                         """
                         if s_tag == 'INDI':
                             num += 1
-                            list.append(['INDI', ID])
+                            line_ID = line_num
+                            list.append(['INDI', ID, line_ID])
                         elif s_tag == 'FAM':
                             num += 1
-                            list.append(['FAM', ID])
-
+                            line_ID = line_num
+                            list.append(['FAM', ID, line_ID])
+                        line_ID = 0
                     else:
                         """
                         num != 0 which means there is already an 'INDI' or 'FAM'
@@ -87,14 +91,16 @@ def parse_GEDCOM(path):
 
                             if s_tag == 'INDI':
                                 num = 1
-                                list.append(['INDI', ID])
+                                line_ID = line_num
+                                list.append(['INDI', ID, line_ID])
 
                             elif s_tag == 'FAM':
                                 num = 1
-                                list.append(['FAM', ID])
+                                line_ID = line_num
+                                list.append(['FAM', ID, line_ID])
                             else:
                                 num = 0
-
+                            line_ID = line_num
                 elif level != '0':
                     tag = list_line[1]
                     del list_line[0: 2]
@@ -106,21 +112,24 @@ def parse_GEDCOM(path):
                     elif date_b == 1:
                         if tag == 'DATE':
                             """ date_b == 1 which means it is birth date """    
-                            list.append(['BIRTH', argument])        
+                            line_BIRTH = line_num
+                            list.append(['BIRTH', argument, line_BIRTH])        
                         else:
-                            list.append(['BIRTH', ''])
-                            list.append([tag, argument])
+                            line_BIRTH = line_num - 1
+                            
+                            list.append(['BIRTH', '', line_BIRTH])
+                            list.append([tag, argument, line_num])
                         date_b = 0
                     elif date_b == 2:
                         if tag == 'DATE':
                             """ date_b == 2 which means it is married date"""
-                            list.append(['MARR', argument])    
+                            list.append(['MARR', argument, line_num])    
                         else:
-                            list.append(['MARR', ''])
-                            list.append([tag, argument])
+                            list.append(['MARR', '', line_num -1])
+                            list.append([tag, argument, line_num])
                         date_b = 0
                     else:
-                        list.append([tag, argument])
+                        list.append([tag, argument, line_num])
                         date_b = 0
                     
             if level != '0':
@@ -132,45 +141,109 @@ def parse_GEDCOM(path):
             for item in sum:
 
                 if item[0][0] == 'INDI':
+                    feat_IND['line_ID'] = item[0][2]
                     m = item.pop(0)
+                    
                     for q in item:
                         if q[0] == 'FAMC':
-                            famc_s.add(q[1])
+                            
+                            famc_s.append([q[1], q[2]])
+
                             feat_IND['FAMC'] = famc_s
                             """ 
                             collect famc in indi level and put them into famc_s set
                             """
                         elif q[0] == 'FAMS':
-                            fams_s.add(q[1])
+                            
+                            fams_s.append([q[1], q[2]])
                             feat_IND['FAMS'] = fams_s
                             """
                             collect fams in indi level and put them into fams_s set
                             """
                         else:
-                            feat_IND[q[0]] = q[1]
+                            feat_IND[q[0]] = [q[1], q[2]]
 
                     dict_indi[m[1]] = feat_IND
                     """ put feat_IND into the result dictionary dict_indi """
-                    famc_s = set()
-                    fams_s = set()
+                    famc_s = []
+                    fams_s = []
                     feat_IND = defaultdict(lambda: 'N/A')
 
                 elif item[0][0] == 'FAM':
+                    feat_FAM['line_ID'] = item[0][2]
                     n = item.pop(0)
                     for q in item:
                         if q[0] == 'CHIL':
-                            child_s.add(q[1])
+                            
+                            child_s.append([q[1], q[2]])
                             feat_FAM['CHIL'] = child_s
                             """
                             collect children in fam level and put them into child_s set
                             """
                         else:
-                            feat_FAM[q[0]] = q[1]
+                            feat_FAM[q[0]] = [q[1], q[2]]
 
                     dict_fam[n[1]] = feat_FAM
                     """ put feat_FAM into the result dictionary dict_fam """
-                    child_s = set()
+                    child_s = []
                     feat_FAM = defaultdict(lambda: 'N/A')
+            
+            for key, value in dict_fam.items():
+                l_item1 = []   
+                if value['DATE'] != 'N/A':
+                    l_child = value['CHIL']
+                    id_wife = value['WIFE'][0]
+                    id_hus = value['HUSB'][0] 
+                    fam_key = key
+                    
+                    for key1, value1 in dict_fam.items():
+                        if value1['HUSB'][0] == id_hus or value1['WIFE'][0] == id_wife:
+                            if key1 != fam_key:
+                                for item1 in value1['CHIL']:
+                                    l_item1.append(item1[0]) 
+                                for item in l_child:
+                                    if item[0] not in l_item1:
+                                        value1['CHIL'].append(item)
+                                    else:
+                                        continue
+                                dict_fam[key1]['CHIL'] = value1['CHIL']
+
+            for key2, value2 in dict_indi.items():
+                l_item2 = []
+                if value2['DATE'] != 'N/A' :
+                    id_death = key2 
+                    for fam in value2['FAMS']:
+                        key_fam = fam[0]
+                        child = dict_fam[key_fam]['CHIL']
+                        for key4, value4 in dict_fam.items():
+                            if id_death == dict_fam[key_fam]['HUSB'][0]:
+                                if dict_fam[key_fam]['WIFE'][0] in [value4['HUSB'][0], value4['WIFE'][0]] and key4 != key_fam:
+                                    for item1 in value4['CHIL']:
+                                        l_item2.append(item1[0])
+                                    for item in child :
+                                        if item[0] not in l_item2:
+                                            value4['CHIL'].append(item)
+                                        else:
+                                            continue
+                                    dict_fam[key4]['CHIL'] = value4['CHIL']
+
+                            elif id_death == dict_fam[key_fam]['WIFE']:
+                               
+                                if dict_fam[key_fam]['HUSB'][0] in [value4['HUSB'][0], value4['WIFE'][0]] and key4 != key_fam:
+                                    for item1 in value4['CHIL']:
+                                        l_item2.append(item1[0])
+                                    for item in child :
+                                        if item[0] not in l_item2:
+                                            value4['CHIL'].append(item)
+                                        else:
+                                            continue
+                                    dict_fam[key4]['CHIL'] = value4['CHIL']
+
+                else:
+                    continue
+        
+
+            
             
             if len(dict_indi) < 5000 and len(dict_fam) < 1000:
                 return dict_indi, dict_fam
@@ -192,49 +265,90 @@ if __name__ == "__main__":
     except ValueError as m:
         print(m)
     else:
+        
         table_indi = PrettyTable(
             ['ID', 'Name', 'Gender', 'Birthday', 'Alive', 'Death', 'Child', 'Spouse'])
         table_fam = PrettyTable(['ID', 'Married', 'Divorced', 'Husband ID',
                                  'Husband Name', 'Wife ID', 'Wife Name', 'Children'])
-
-        for key, value in indi.items():            
+        l_FAMC = []
+        l_FAMS = []
+        l_CHIL = []
+        famc = 'N/A'
+        fams = 'N/A'
+        chil = 'N/A'
+        
+        for key, value in indi.items():  
+            DATE_DEATH = 'N/A'       
             if value['DEAT'] == 'N/A':
                 alive = 'TRUE'
             else:
-                if value['DATE'] not in ['', 'N/A']:
+                if value['DATE'][0] not in ['', 'N']:
                     alive = 'FALSE'
+                    DATE_DEATH = value['DATE'][0]
                 else:   
                     alive = 'FALSE'
-                    value['DATE'] = ''
-                
-            table_indi.add_row([key, value['NAME'], value['SEX'], value['BIRTH'],
-                                alive, value['DATE'], value['FAMC'], value['FAMS']])
+                    DATE_DEATH = ''
+            
+            if value['FAMC'] == 'N/A':
+                famc = 'N/A'
+            else:    
+                for famc in value['FAMC']:
+                    l_FAMC.append(famc[0])
+                famc = l_FAMC
+            
+            if value['FAMS'] == 'N/A':
+                fams = 'N/A'
+            else:
+                for fams in value['FAMS']:
+                    l_FAMS.append(fams[0]) 
+                fams = l_FAMS
+            
+            table_indi.add_row([key, value['NAME'][0], value['SEX'][0], value['BIRTH'][0],
+                                alive, DATE_DEATH, famc, fams])
+            l_FAMC = []
+            l_FAMS = []
 
         for key, value in fam.items():
-            id_h = value['HUSB']
-            id_w = value['WIFE']
+
+            DATE_DIV = 'N/A' 
+            id_h = value['HUSB'][0]
+            id_w = value['WIFE'][0]
             """ husband id and wife id """
-            if id_h not in ['N/A', '']:
-                name_h = indi[id_h]['NAME']
+            if id_h not in ['N', '']:
+                name_h = indi[id_h]['NAME'][0]
             else:
                 name_h = 'N/A'
             """ get husband's name"""
-            if id_w not in['N/A', '']:
-                name_w = indi[id_w]['NAME']
+            if id_w not in['N', '']:
+                name_w = indi[id_w]['NAME'][0]
             else:
                 name_w = 'N/A'
             """ get wife's name"""
 
             if value['DIV'] == 'N/A':
-                value['DATE'] = 'N/A'
-            elif value['DATE'] in ['', 'N/A']:
-                value['DATE'] = ''
+                
+                DATE_DIV = 'N/A'
+            else:
+                if value['DATE'][0] in ['', 'N']:
+                    DATE_DIV = ''
+                else:
+                    DATE_DIV = value['DATE'][0]
             """ 
             if div record lost, print''
             if no divorce, print'N/A'
             """
-            table_fam.add_row([key, value['MARR'], value['DATE'],
-                               id_h, name_h, id_w, name_w, value['CHIL']])
+            
+            if value['CHIL'] == 'N/A':
+                chil = 'N/A'
+            else:
+                for chil in value['CHIL']:
+                    l_CHIL.append(chil[0]) 
+                chil = l_CHIL
+            table_fam.add_row([key, value['MARR'][0], DATE_DIV,
+                               id_h, name_h, id_w, name_w, chil])
+        
+            l_CHIL = []
+        
         print(table_indi)
         print(table_fam)
 
@@ -244,5 +358,10 @@ if __name__ == "__main__":
             print(e)
         try:
             US0203_xye.US03(indi)
+        except ValueError as e:
+            print(e)
+
+        try:
+            US12.US12(indi, fam)
         except ValueError as e:
             print(e)
