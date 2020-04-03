0a92b0e87ac772f3795d67c2644cf8d8e020136c
jeny-xye <57339127+jeny-xye@users.noreply.github.com>
2020-04-03 16:37:07 -0400

    update output_errors.py

--- gedcom_app/view/output_errors.py
+++ gedcom_app/view/output_errors.py
@@ -17,24 +17,20 @@ def output_errors_fam(fam):
     :param fam:
     :return:
     """
-    fam_err=[]
     for family in fam.values():
         error_list = family.error_list
         if len(error_list) != 0:
             for error in error_list:
-                fam_err.append(f"{error.error_info['error_type']}: {error.error_info['indi_or_fam']}: "
+                print(f"{error.error_info['error_type']}: {error.error_info['indi_or_fam']}: "
                       f"{error.error_info['user_story']}: {error.error_info['error_line']}: "
                       f"{error.error_info['entity_id']}: {error.error_message}")
-    return fam_err
 
 
 def output_errors_indi(indi):
-    indi_err=[]
     for indi in indi.values():
         error_list = indi.error_list
         if len(error_list) != 0:
             for error in error_list:
-                indi_err.append(f"{error.error_info['error_type']}: {error.error_info['indi_or_fam']}: "
+                print(f"{error.error_info['error_type']}: {error.error_info['indi_or_fam']}: "
                       f"{error.error_info['user_story']}: {error.error_info['error_line']}: "
                       f"{error.error_info['entity_id']}: {error.error_message}")
-    return indi_err
