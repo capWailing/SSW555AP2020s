24f653da014b0ff606ed26087425a79993c9dc9b
jeny-xye <57339127+jeny-xye@users.noreply.github.com>
2020-04-03 16:28:28 -0400

    change version of startup.py

--- gedcom_app/startup.py
+++ gedcom_app/startup.py
@@ -9,7 +9,6 @@ from gedcom_app.view.output_errors import output_errors_indi, output_errors_fam
 from gedcom_app.view.output_prettytable import individual_table, family_table
 from gedcom_app.control.build_entity import build_individual, build_family
 from gedcom_app.control.verification import verification
-import sys
 
 
 def main():
@@ -20,20 +19,11 @@ def main():
     dict_indi, dict_fam = parse_gedcom(path)
     indi_list = build_individual(dict_indi)
     fam_list = build_family(dict_fam, indi_list)
-    indi=individual_table(indi_list)
-    fam=family_table(fam_list)
+    individual_table(indi_list)
+    family_table(fam_list)
     verification(indi_list, fam_list)
-    indi_err=output_errors_indi(indi_list)
-    fam_err=output_errors_fam(fam_list)
-    #sys.stdout = open('sprint2_test.log', mode='w', encoding='utf-8')
-    #上面这句语句是用来打印这周输出的
-    print(indi)
-    print(fam)
-    for i in indi_err:
-        print(i)
-
-    for i in fam_err:
-        print(i)
+    output_errors_indi(indi_list)
+    output_errors_fam(fam_list)
 
 
 if __name__ == '__main__':
