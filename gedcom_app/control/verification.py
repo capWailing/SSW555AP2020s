a6909c1ea820b36cde9424f9037364982e54508d
capWailing <thomasyantom@126.com>
2020-04-03 20:08:59 -0400

    update us 22 and us 15

--- gedcom_app/control/verification.py
+++ gedcom_app/control/verification.py
@@ -3,6 +3,7 @@
     description: this starts the verification part
     date: 3/2/2020
 """
+from gedcom_app.control.US15 import multiple_siblings
 from gedcom_app.control.child_birth import birth_before_marriage
 from gedcom_app.control.us07 import less_than_150
 from gedcom_app.control.us14 import multiple_birth
@@ -31,3 +32,4 @@ def verification(indi_dict, fam_dict):
     USO4(fam_dict)
     US05(fam_dict)
     US11(indi_dict, fam_dict)
+    multiple_siblings(fam_dict)
