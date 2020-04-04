edd5981c81b801c3e33b1222b92e87b15e1c799f
huangxiaoqiang <adamhzq1996@gmail.com>
2020-04-04 17:27:22 -0400

    US19 and US23

--- gedcom_app/tests/test_us1923.py
+++ gedcom_app/tests/test_us1923.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 import unittest
-from gedcom_app.control.US1923 import first_cousin_should_not_marry_us19, unique_date_and_birthday_us23
+from gedcom_app.control.US1923 import first_cousin_should_not_marry_us19, unique_name_and_birthday_us23
 from gedcom_app.tests.build_instance import build_family_list, build_individual_list
 
 path1 = r"..\..\test19.ged"
@@ -16,7 +16,7 @@ class Test(TestCase):
 
     def test_unique_date_and_birthday_us23(self):
         individual_dict = build_individual_list(path2)
-        unique_date_and_birthday_us23(individual_dict)
+        unique_name_and_birthday_us23(individual_dict)
         self.assertEqual([str(error) for error in individual_dict['023I01'].error_list],
                          ['Error: INDIVIDUAL: US23: 3: 023I01: more than one individual with same name '
                           'Qiang /Wang/ and birth date 5 JUL 1950 appear in the GED file'])
